7ebf894db64fe9017512f42be560d8a76d593a56
fran <125706199+YooPuiPui@users.noreply.github.com>
2025-03-26 22:23:49 -0300

    finished

--- Calculadora.py
+++ Calculadora.py
@@ -13,3 +13,24 @@ numero1 = int(input())
 print("\nIngrese el segundo numero:")
 numero2 = int(input())
 
+# Sumaremos los numeros 
+
+suma = numero1 + numero2
+
+print(f"El valor de la suma es: {suma}")
+
+resta = numero1 - numero2
+
+print(f"El valor de la resta es: {resta}")
+
+mul = numero1*numero2
+print(f"La multiplicacion es: {mul}")
+
+print("\nDivision")
+print("\nIngrese el primer valor a dividir:")
+x = int(input())
+print("\nIngrese el segundo valor a dividir:")
+y = int(input())
+
+div = x/y
+print(f"El valor de la division es: {div} ")
