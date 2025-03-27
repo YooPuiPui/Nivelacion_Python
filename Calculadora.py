64ff3563cc605521dc14e837cece26341636d05d
fran <125706199+YooPuiPui@users.noreply.github.com>
2025-03-26 22:14:32 -0300

    add

---
+++ Calculadora.py
@@ -0,0 +1,15 @@
+#titulo 
+print("         CALCULADORA         ")
+print("Ingrese las variables para calcular su suma, resta y multiplicacion.")
+
+# Ingresemos el primer numero
+print("\nIngrese el primer numero:")
+
+# Y lo guardamos en la variable llamada numero1
+
+numero1 = int(input())
+
+# Segundo numero
+print("\nIngrese el segundo numero:")
+numero2 = int(input())
+
